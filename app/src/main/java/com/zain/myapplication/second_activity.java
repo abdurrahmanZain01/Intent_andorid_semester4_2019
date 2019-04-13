b4067b0be139a636a314aa83fe0d94e2878e0af0
zain1731710168MI2E <penyimpanan05@gmail.com>
2019-04-13 15:32:02 +0700

    Intent_android_semester4_2019

---
+++ app/src/main/java/com/zain/myapplication/second_activity.java
@@ -0,0 +1,18 @@
+package com.zain.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+//    public void launchSecondActivity(View view){
+//        Log.d(LOG_TAG, "Button diklik");
+//
+//    }
+}
